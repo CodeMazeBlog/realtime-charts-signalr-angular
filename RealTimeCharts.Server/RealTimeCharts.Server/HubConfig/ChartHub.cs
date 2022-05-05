7b955608bc5e528c20ddb8b08312445c3d8097c4
MarinkoSpasojevic <marinkospasojevic@gmail.com>
2022-05-05 11:55:25 +0200

    Updated both projects

---
+++ RealTimeCharts.Server/RealTimeCharts.Server/HubConfig/ChartHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using RealTimeCharts.Server.Models;
+
+namespace RealTimeCharts.Server.HubConfig
+{
+    public class ChartHub : Hub
+    {
+        public async Task BroadcastChartData(List<ChartModel> data) => 
+            await Clients.All.SendAsync("broadcastchartdata", data);
+    }
+}
